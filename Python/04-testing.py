6ee2aeb6f6057d4da1cd2f517304af64b51de470
Rohit kumar <rohitkumar9643017@gmail.com>
2026-09-17 23:20:36 +0530

    asdfghjkl

---
+++ Python/04-testing.py
@@ -0,0 +1,5 @@
+c=10
+print(f"The value of c is: {c}")
+print("This is a test for Python code execution.")
+a="Hello, World!"
+print(a)
---
+++ Python/05-testing5.py
@@ -0,0 +1,3 @@
+c=2
+b=20
+print(f"The sum of c and b is: {c+b}")
